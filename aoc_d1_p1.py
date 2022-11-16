fcf1ea282e52837480297bfb9956a85772de305a
kadamson615 <49322307+kadamson615@users.noreply.github.com>
2022-11-16 14:06:51 -0500

    Update aoc_d1_p1.py
    deleted part of data upload criteria

--- aoc_d1_p1.py
+++ aoc_d1_p1.py
@@ -1,4 +1,4 @@
-data = open('/Users/kayleighadamson/Desktop/aoc_d1_p1.txt', 'r', encoding='utf-8').read().splitlines()
+data = open('/Users/kayleighadamson/Desktop/aoc_d1_p1.txt', 'r')
 depths = [int(x) for x in data]
 increases = sum(x < y for x, y in zip(depths, depths[1:]))
 print(increases)
